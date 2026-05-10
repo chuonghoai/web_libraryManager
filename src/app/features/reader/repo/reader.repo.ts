2db8925ce9a08f9b15543ddb7d48288ec92337e0
chuonghoai <144915081+chuonghoai@users.noreply.github.com>
2026-05-10 19:31:31 +0700

    fix: remove mock repo and add real api call in module reader

--- src/app/features/reader/repo/reader.repo.ts
+++ src/app/features/reader/repo/reader.repo.ts
@@ -16,29 +16,7 @@ export class ReaderRepo {
      * @returns List readers
      */
     getAllReaders(): Observable<ApiResponse<Reader[]>> {
-        // return this.http.get<ApiResponse<Reader[]>>(this.apiUrl);
-        return of({
-            success: true,
-            message: 'Lấy danh sách độc giả thành công',
-            data: [
-                {
-                    id: '1',
-                    name: 'Nguyễn Văn A',
-                    phone: '0123456789',
-                    code: 'R001',
-                    createdAt: '2024-01-01',
-                    updatedAt: '2024-01-01',
-                },
-                {
-                    id: '2',
-                    name: 'Nguyễn Văn B',
-                    phone: '0123456789',
-                    code: 'R002',
-                    createdAt: '2024-01-01',
-                    updatedAt: '2024-01-01',
-                },
-            ],
-        });
+        return this.http.get<ApiResponse<Reader[]>>(this.apiUrl);
     }
 
     /**
@@ -47,19 +25,7 @@ export class ReaderRepo {
      * @return Reader
      */
     getById(id: string): Observable<ApiResponse<Reader>> {
-        // return this.http.get<ApiResponse<Reader>>(`${this.apiUrl}/${id}`);
-        return of({
-            success: true,
-            message: `Lấy thông tin độc giả ${id} thành công`,
-            data: {
-                id: id,
-                name: 'Nguyễn Văn A',
-                phone: '0123456789',
-                code: 'R001',
-                createdAt: '2024-01-01',
-                updatedAt: '2024-01-01',
-            },
-        });
+        return this.http.get<ApiResponse<Reader>>(`${this.apiUrl}/${id}`);
     }
 
     /**
@@ -68,19 +34,7 @@ export class ReaderRepo {
      * @return Reader
      */
     create(dto: CreateReaderDto): Observable<ApiResponse<Reader>> {
-        // return this.http.post<ApiResponse<Reader>>(this.apiUrl, dto);
-        return of({
-            success: true,
-            message: 'Thêm độc giả thành công',
-            data: {
-                id: Date.now().toString(),
-                name: dto.name,
-                phone: dto.phone,
-                code: 'R003',
-                createdAt: new Date().toISOString(),
-                updatedAt: new Date().toISOString(),
-            },
-        });
+        return this.http.post<ApiResponse<Reader>>(this.apiUrl, dto);
     }
 
     /**
@@ -90,19 +44,7 @@ export class ReaderRepo {
      * @returns Reader
      */
     update(id: string, dto: CreateReaderDto): Observable<ApiResponse<Reader>> {
-        // return this.http.put<ApiResponse<Reader>>(`${this.apiUrl}/${id}`, dto);
-        return of({
-            success: true,
-            message: 'Cập nhật độc giả thành công',
-            data: {
-                id: id,
-                name: dto.name,
-                phone: dto.phone,
-                code: 'R003',
-                createdAt: new Date().toISOString(),
-                updatedAt: new Date().toISOString(),
-            },
-        });
+        return this.http.put<ApiResponse<Reader>>(`${this.apiUrl}/${id}`, dto);
     }
 
     /**
@@ -111,11 +53,6 @@ export class ReaderRepo {
      * @returns null
      */
     delete(id: string): Observable<ApiResponse<null>> {
-        // return this.http.delete<ApiResponse<null>>(`${this.apiUrl}/${id}`);
-        return of({
-            success: true,
-            message: 'Xóa độc giả thành công',
-            data: null,
-        });
+        return this.http.delete<ApiResponse<null>>(`${this.apiUrl}/${id}`);
     }
 }
